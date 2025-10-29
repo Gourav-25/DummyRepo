e20e882c3e747500e3893ea6f0a8810e262b0aeb
Gourav-25 <gaman411@gmail.com>
2025-10-29 12:40:23 +0530

    Update TestJavasdxfcgv.java

--- src/test/java/TestJava.java
+++ src/test/java/TestJava.java
@@ -8,8 +8,7 @@ public class TestJava {
 	System.out.println("test4");
 	System.out.println("test5");
     System.out.println("test6");
-	System.out.println("test7");	
-		System.out.println("test81");
+	System.out.println("cfvhb");
 	}
 
 }
